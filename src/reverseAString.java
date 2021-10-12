0c8e5059c9a0969aafa3e8bfcb3e516c24f1b072
RoVibes <74771645+RoVibes@users.noreply.github.com>
2021-10-13 00:33:45 +0300

    Reverse a string with StringBuffer

--- src/reverseAString.java
+++ src/reverseAString.java
@@ -15,18 +15,18 @@ public class reverseAString {
 		StringBuffer strnormal = new StringBuffer(str);
 		
 		for (int i = 0; i < str.length(); i++) {
-			chars[i] = strnormal.charAt(i);
+			chars[i] = strnormal.charAt(i);					//Get all the chars from the string and save it in an array of chars
         }
 		
-		int capacity = str.length();
-		StringBuffer strreversed = new StringBuffer(capacity);
+		int capacity = str.length();						
+		StringBuffer strreversed = new StringBuffer(capacity);		//Set capacity for new string for the new reversed StringBuffer
+		
 		for(int i = capacity - 1; i >= 0; i--)
 		{
-			strreversed.append(chars[i]);
+			strreversed.append(chars[i]);					//Append all the chars from the string to the new StringBuffer in reverse order.
 		}
 		
-		System.out.print(strreversed);
-
+		System.out.print(strreversed); 
 	}
 
 }
