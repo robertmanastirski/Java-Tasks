e63c14bac211f699c563935b1459b3607e47e253
RoVibes <robertmwrk@gmail.com>
2021-10-19 18:11:29 +0300

    Update SymmetricalNumber.java

--- Symmetrical Number/SymmetricalNumber.java
+++ Symmetrical Number/SymmetricalNumber.java
@@ -5,13 +5,13 @@ public class SymmetricalNumber {
 	public static void Check(int num)
 	{
 		int a1,a2,a3;
-		a3 = num % 10;
+		a3 = num % 10;			//Get the remainder from dividing the number to 10
 	    num = num / 10;
 	    a2 = num % 10;
 	    num = num / 10;
 	    a1 = num % 10;
 	    num = num / 10;
-	    if(a3 == a1)
+	    if(a3 == a1)		   //Since our input should be only with 3 digit numbers we check if the first and last digits of the number are the same. If they are that means that the number is symmetrical.
 	    {
 	    	System.out.println("YES");
 	    }else
