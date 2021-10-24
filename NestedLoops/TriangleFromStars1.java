c21beedc17ce4cb6643422b3602d9b5b5138301e
RoVibes <robertmwrk@gmail.com>
2021-10-24 16:03:45 +0300

    Create a triangle from stars 1
    Create a triangle from stars 1

---
+++ NestedLoops/TriangleFromStars1.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class TriangleFromStars1 {
+    
+    public static void main(String[] args) {
+	int N = 0;
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Input N: ");
+	N = sc.nextInt();
+	
+	for(int i = 0; i < N; i++)
+	{
+            for(int j = 0; j <= i; j++)
+            {
+                System.out.print("*");
+            }
+                
+            System.out.println();
+        }
+
+	}
+}
